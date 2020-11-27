20af631a486af9a94553e246d8d32f22d70ff33e
jianengli <h19jiali@du.se>
2020-11-27 21:14:42 +0800

    update 141 problem

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 | ID | Title | Difficulty | Solution
 | :------------ | :------------ | :------------ | :------------ |
 | 24 | [Swap Nodes in Pairs](https://leetcode.com/problems/swap-nodes-in-pairs/) | Easy | [LinkedList](linkedList/24.py)|
+| 141 | [Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/) | Easy | [LinkedList](linkedList/141.py)|
 | 206 | [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/) | Easy | [LinkedList](linkedList/206.py)|
---
+++ linkedList/141.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = fast = head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+            
+        return False
+
+    # 1. do not forget to check whether fast.next exists
